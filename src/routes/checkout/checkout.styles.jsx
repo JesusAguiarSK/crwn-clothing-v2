5a575b253005c4665de9f1b160963a3fbecb1f37
JesusAguiarSK <j.aguiar@softwarekeep.com>
2023-06-22 08:10:15 -0400

    Checkout styling

--- src/routes/checkout/checkout.styles.jsx
+++ src/routes/checkout/checkout.styles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const CheckoutContainer = styled.div`
-  width: 55%;
+  width: 100%;
   min-height: 90vh;
   display: flex;
   flex-direction: column;
@@ -29,5 +29,5 @@ export const HeaderBlock = styled.div`
 export const Total = styled.span`
   margin-top: 30px;
   margin-left: auto;
-  font-size: 36px;
+  font-size: 26px;
 `;
